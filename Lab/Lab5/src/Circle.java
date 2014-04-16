b109995a395054d69152be77a1298a484c52ce44
COEUScreatesKRATOS <ChrisHardingBrown@vlan810-037-176.pioneernet.du.edu>
2014-04-15 18:28:43 -0600

    Starting Classes

---
+++ Lab/Lab5/src/Circle.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ChrisHardingBrown
+ *
+ */
+public class Circle {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ Lab/Lab5/src/Jungle.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ChrisHardingBrown
+ *
+ */
+public class Jungle {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
