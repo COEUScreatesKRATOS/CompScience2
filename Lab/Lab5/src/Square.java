5327e28831003db7507a030cc4647324976fb0df
COEUScreatesKRATOS <ChrisHardingBrown@vlan810-037-176.pioneernet.du.edu>
2014-04-16 20:28:38 -0600

    Working project

--- Lab/Lab5/src/Square.java
+++ Lab/Lab5/src/Square.java
@@ -4,16 +4,57 @@
 
 /**
  * @author ChrisHardingBrown
- *
+ * 
  */
 public class Square {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    private int myX;
+    private final int myY;
+    private int myXVel;
+    private final int myWidth;
+    private final int myHeight;
+    public final double distancetocorner;
 
+    public Square(final int x, final int y) {
+        myX = x;
+        myY = y;
+        myXVel = 5;
+        myWidth = 10;
+        myHeight = 10;
+        distancetocorner = Math.sqrt(Math.pow(myWidth / 2.0, 2) + Math.pow(myHeight / 2.0, 2));
+
+    }
+
+    public void draw() {
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.filledRectangle(myX, myY, myWidth, myHeight);
+    }
+
+    public void move() {
+        if (myX < myWidth / 2) {
+            myXVel *= -1;
+            myX = myWidth / 2;
+        } else if (myX > 500 - myWidth / 2) {
+            myXVel *= -1;
+            myX = (500 - myWidth / 2);
+        }
+        myX += myXVel;
+    }
+
+    public int getMyX() {
+        return myX;
+    }
+
+    public int getMyY() {
+        return myY;
+    }
+
+    public int getMyWidth() {
+        return myWidth;
+    }
+
+    public int getMyHeight() {
+        return myHeight;
     }
 
 }
